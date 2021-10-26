c16f09ddafe39dc13942aa024578c60bc0892597
Oscar Goos <wogoos@users.noreply.github.com>
2021-10-26 11:15:38 -0500

    Add files via upload

--- lib/HC_RS04_Driver/src/HC_SR04_Driver.cpp
+++ lib/HC_RS04_Driver/src/HC_SR04_Driver.cpp
@@ -1,35 +1,33 @@
 #include <HC_SR04_Driver.h>
 
-HC_SR04::HC_SR04(uint8_t trig, uint8_t echo, uint8_t temp, uint8_t FIR, uint16_t FIRstart){
-    air_temp = temp;
-    trigPin  = trig;
-    echoPin  = echo;
-    dist_FIR = FIRstart;
+HC_SR04::HC_SR04(uint8_t trig, uint8_t echo, uint8_t temp, uint8_t FIRfac, uint16_t noise){
+    air_temp     = temp;
+    trigPin      = trig;
+    echoPin      = echo;
+    noise_margin = noise;
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
     digitalWrite(trigPin, LOW);
     cal= (331.3 + 0.606*air_temp)/(2*1000);
-    FIRfactor=FIR;
+    FIRfactor=FIRfac;
+}
+
+void     HC_SR04::init(){
+    dist_FIR=distance();                // init the startvalue of dist_FIR
+} 
+
+uint16_t HC_SR04::distance(uint8_t temp){
+    air_temp=temp;
+    cal = (331.3 + 0.606*air_temp)/(2*1000);
+    return distance();
 }
 
 uint16_t HC_SR04::distance(){
-    return distance(air_temp, false);
-    }
-
-uint16_t HC_SR04::distance(uint8_t temp){
-    return distance(temp, false);
-}
-
-uint16_t HC_SR04::distance(uint8_t temp, boolean FIR){
-    if (temp != air_temp) {
-      air_temp=temp;
-      cal = (331.3 + 0.606*air_temp)/(2*1000);
-    }
     digitalWrite(trigPin, HIGH);         // Trigger the sensor by setting the trigPin high for 10 microseconds:
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);  
-    dist= cal*pulseIn(echoPin, HIGH);    // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
-    dist_FIR+= (dist - dist_FIR)/FIRfactor;
-    if (FIR) return  (uint16_t) dist_FIR;   // Return distance in mm.
-    else     return  (uint16_t) dist; 
+    dist = cal*pulseIn(echoPin, HIGH);    // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
+    if ((uint16_t)(abs(dist-dist_FIR))>noise_margin) dist_FIR=dist;
+    dist_FIR += (dist - dist_FIR)/FIRfactor;
+    return  (uint16_t) dist;
 }
--- lib/HC_RS04_Driver/src/HC_SR04_Driver.h
+++ lib/HC_RS04_Driver/src/HC_SR04_Driver.h
@@ -6,15 +6,14 @@ Header file definitions for HC-SR04 typ sensors
 #ifndef  HC_SR04_Driver_H
 #define  HC_SR04_Driver_H
 
-
 class  HC_SR04 {
 
 public:
             HC_SR04 (uint8_t, uint8_t, uint8_t, uint8_t, uint16_t);
-
-uint16_t    distance(uint8_t, boolean);
 uint16_t    distance(uint8_t);
 uint16_t    distance();
+void        init();
+float       dist, dist_FIR; 
 
 private:
 uint8_t     air_temp;
@@ -22,8 +21,8 @@ uint8_t     trigPin;
 uint8_t     echoPin;
 uint16_t    TOF;                          // Time Of Flight
 float       cal;                          // Vsound=(331.3 + 0.606T)/1000 mm/uS, Cal=Vsoudn/2
-float       dist, dist_FIR=100;           //
-uint8_t     FIRfactor;
+uint16_t    noise_margin;                 // Noise margin on the sensor measured value depends on envirnement
+uint8_t     FIRfactor;                    // factor usedby the FIR filter to avarage the output value
 
 protected:
 };
