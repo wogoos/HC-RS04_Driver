8f9e0cdd9a3cd693488d2efb4377e8741ce1ffef
Oscar Goos <wogoos@users.noreply.github.com>
2021-10-26 11:18:53 -0500

    Add files via upload

--- Example/main.cpp
+++ Example/main.cpp
@@ -4,18 +4,25 @@
 /* Example code for HC-SR04 ultrasonic distance sensor with Arduino. No library required. More info: https://www.makerguides.com */
 // Define Trig and Echo pin:
 
-HC_SR04   DistSens(2,3,30,10);            // TrigPin, EchoPin, air temperature, FIRfactor.
+HC_SR04   DistSens(2,3,22,10,100);            // TrigPin, EchoPin, air_temperature, FIRfactor
 
 void setup() {
     // Define inputs and outputs:
     Serial.begin(115200);
+    DistSens.init();
 }
 void loop() {
+    DistSens.distance(20);
     Serial.print("Distance = ");
-    Serial.print( DistSens.distance());
+    Serial.print( DistSens.dist);
     Serial.print(",  ");
-    Serial.print( DistSens.distance(30));
+    Serial.print( (uint16_t)DistSens.dist);
     Serial.print(",  ");
-    Serial.print( DistSens.distance(30, true));
-    Serial.println(" cm");
+ //   Serial.print( DistSens.distance(20));
+ //   Serial.print(",  ");
+    Serial.print( DistSens.dist_FIR);
+    Serial.print(",  ");
+    Serial.print( (uint16_t)DistSens.dist_FIR);
+    Serial.println(" mm");
+    delay(1000);
 }
